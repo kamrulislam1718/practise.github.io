7afe4fd4770057446a30dc9558ab9c376417774e
kamrulislam1718 <31001377+kamrulislam1718@users.noreply.github.com>
2017-08-19 16:19:34 +0600

    class-three

---
+++ psdtohtml/class-three/assets/js/scripts.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($){
+	  $(".homepage-slides").owlCarousel({
+	  	items: 1,
+	  	loop: true,
+	  	nav: true,
+	  	dots: true,
+	  	navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+	  	autoplay: false,
+		smartSpeed: 1000
+	  });
+});
