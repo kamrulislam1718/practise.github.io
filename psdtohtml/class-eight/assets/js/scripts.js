18edc545a458cfd17cd737b6a484b279bde2a834
kamrulislam1718 <31001377+kamrulislam1718@users.noreply.github.com>
2017-09-19 23:08:36 +0600

    class-eight

---
+++ psdtohtml/class-eight/assets/js/scripts.js
@@ -0,0 +1,65 @@
+jQuery(document).ready(function($){
+	  $(".homepage-slides").owlCarousel({
+	  	items: 1,
+	  	loop: true,
+	  	nav: true,
+	  	dots: true,
+	  	navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+	  	autoplay: false,
+		smartSpeed: 1000
+	  });
+    $(".team-list").owlCarousel({
+      items: 3,
+      margin: 30,
+      loop: true,
+      nav: false,
+      dots: true,
+      autoplay: false,
+    });
+
+    $(".testimonial-list").owlCarousel({
+      items: 3,
+      margin: 30,
+      loop: true,
+      nav: false,
+      dots: true,
+      autoplay: false,
+    });
+
+    $(".logo-carousel").owlCarousel({
+      items: 5,
+      margin: 30,
+      loop: true,
+      nav: false,
+      dots: false,
+      autoplay: false,
+    });
+    
+	  $('#web-design-skillbar').circleProgress({
+    value: 0.90,
+    size: 270,
+    thickness: 2,
+    fill: '#0ABBC1',
+  }).on('circle-animation-progress', function(event, progress) {
+    $(this).find('.skill-count-no').html(Math.round(90 * progress) + '<i>%</i>');
+  });
+
+  $('#graphic-design-skillbar').circleProgress({
+    value: 0.95,
+    size: 270,
+    thickness: 2,
+    fill: '#0ABBC1',
+  }).on('circle-animation-progress', function(event, progress) {
+    $(this).find('.skill-count-no').html(Math.round(95 * progress) + '<i>%</i>');
+  });
+
+  $('#digital-marketing-skillbar').circleProgress({
+    value: 0.84,
+    size: 270,
+    thickness: 2,
+    fill: '#0ABBC1',
+  }).on('circle-animation-progress', function(event, progress) {
+    $(this).find('.skill-count-no').html(Math.round(84 * progress) + '<i>%</i>');
+  });
+
+});
